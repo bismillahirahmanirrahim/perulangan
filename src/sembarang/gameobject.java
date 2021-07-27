d1ee088f7af10a4f338ad5cd35243fb3694961eb
bismillahirahmanirrahim <muhammad_rafi'firdaus_29rpl@student.smktelkom-mlg.sch.id>
2021-07-27 17:29:25 +0700

    perulangan

---
+++ src/sembarang/gameobject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sembarang;
+
+/**
+ *
+ * @author PC
+ */
+public class gameobject {
+        int hp,damage;
+        String nama,skin;
+        
+        public void jalan (String jalan){
+            
+        } 
+        
+    
+}
